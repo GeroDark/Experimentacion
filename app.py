599517ab50065f3eae666d34b63ea33c002afb72
GeroDark <lxlgeraldlxl@gmail.com>
2023-04-03 23:54:28 -0500

    cambio key y git ignore

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 import openai
+import os
 
 app = Flask(__name__)
 
-openai.api_key = "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"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+def generar_nota_periodistica_2(tipo, datos, angulo, fuente, descargo, testimonios, rango_caracteres,contexto):
+    response=openai.ChatCompletion.create(
+        model="gpt-4",
+        max_tokens=1000,
+        n=1,
+        stop=None,
+        temperature=0.8,
+        messages=[
+            {"role": "system", "content": "Tu eres un redactor de notas periodisticas"},
+            {"role": "user", "content": f"Redacta una {tipo} profesional en un rango de {rango_caracteres} caracteres para un diario serio utilizando la siguiente información:\n\nDatos: {datos}\nAngulo: {angulo}\nFuente: {fuente}\nDescargo: {descargo}\nTestimonios: {testimonios}\n\nTitular:\n{{titulo}}\n\nNota periodística:\n{{contenido}}"},
+            
+        ]
+    )
+    return response['choices'][0]['message']['content']
 
 @app.route('/')
 def index():
